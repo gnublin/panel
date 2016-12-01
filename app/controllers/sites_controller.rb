1d52e3ca8a90d5b483b7dd676693851fd918096b
Gauthier FRANCOIS <gauthier.francois@doctolib.fr>
2016-12-01 18:55:40 +0100

    feat(sites): add CRUD

---
+++ app/controllers/sites_controller.rb
@@ -0,0 +1,64 @@
+class SitesController < ApplicationController
+  before_action :set_site, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @sites = params[:all] == 'true' ? Site.all : Site.where(active: true)
+
+  end
+
+  def show
+  end
+
+  def new
+    @site = Site.new
+  end
+
+  def edit
+    @site = Site.find(params[:id])
+  end
+
+  def create
+    @site = Site.new(site_params)
+
+    respond_to do |format|
+      if @site.save
+        format.html { redirect_to root_path, notice: "Site #{@site['name']} was successfully created." }
+        format.json { render :show, status: :created, location: @site }
+      else
+        format.html { render :new }
+        format.json { render json: @site.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def update
+    respond_to do |format|
+      if @site.update(site_params)
+        format.html { redirect_to root_path, notice: "Site #{@site['name']} was successfully updated." }
+        format.json { render :show, status: :ok, location: @site }
+      else
+        format.html { render :edit }
+        format.json { render json: @site.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+    @site.destroy
+    respond_to do |format|
+      format.html { redirect_to sites_url, notice: "Site #{@site['name']} was successfully destroyed." }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_site
+      @site = Site.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def site_params
+      params.require(:site).permit(:name, :url, :active)
+    end
+end
