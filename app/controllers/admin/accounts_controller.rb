777e05126fa1865e9524743d61a06c6aff5cc3f0
Gauthier FRANCOIS <gauthier.francois@doctolib.fr>
2016-12-15 08:28:46 +0100

    Add some account power managment

--- app/controllers/admin/accounts_controller.rb
+++ app/controllers/admin/accounts_controller.rb
@@ -11,9 +11,22 @@ class Admin::AccountsController < ApplicationController
   end
 
   def new
+    @user = User.new
   end
 
   def create
+    p params
+    @user = User.new(user_params)
+
+    respond_to do |format|
+      if @user.save
+        format.html { redirect_to admin_account_path(@user), notice: "user #{@user['email']} was successfully created." }
+        format.json { render :show, status: :created, location: @user }
+      else
+        format.html { render :new }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
   def edit
@@ -53,6 +66,6 @@ class Admin::AccountsController < ApplicationController
     end
 
     def user_params
-      params.require(:user).permit(:email, :admin)
+      params.require(:user).permit(:email, :password, :admin)
     end
 end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!
+  before_action :user_is_an_admin
+
+  private
+  def user_is_an_admin
+    @is_an_admin = false
+    if current_user && current_user.admin
+      @is_an_admin = true
+    end
+  end
 end
