87e52383e3d919562ad33e1c7a5819287d28f191
Gauthier FRANÇOIS <gauthier@openux.org>
2017-02-06 08:07:27 +0100

    some feat on parser

--- app/jobs/make_stats_job.rb
+++ app/jobs/make_stats_job.rb
@@ -2,18 +2,37 @@ class MakeStatsJob < ApplicationJob
   queue_as :default
   def perform(url)
     Resque.logger = Logger.new(STDOUT)
-    Resque.logger.info "#{Time.now}: Message arrived"
+    Resque.logger.info "----------------"
+    Resque.logger.info "Message arrived"
     new_hash = {}
-    offenders_mapping = {time: ['redirects', 'fastestResponse', 'slowestResponse', 'biggestLatency', 'timeToFirstCss', 'smallestLatency', 'timeToFirstJs']}
+    offenders_mapping = {
+      time: [
+        'redirects', 'fastestResponse', 'slowestResponse', 'biggestLatency',
+        'timeToFirstCss', 'smallestLatency', 'timeToFirstJs'
+        ],
+      orig: [
+        'jQueryEventTriggers', 'jQueryDOMWrites', 'DOMqueriesByQuerySelectorAll',
+        'DOMmutationsInserts', 'globalVariables', 'domainsWithCookies', 'DOMinserts',
+        'DOMqueriesByTagName', 'jQueryVersionsLoaded', 'domainsToDomComplete',
+        'DOMqueriesDuplicated', 'DOMmutationsRemoves', 'domainsToDomContentLoaded',
+        'jQueryDOMWriteReadSwitches', 'DOMelementMaxDepth', 'jQueryOnDOMReadyFunctions',
+        'domains', 'DOMmutationsAttributes', 'eventsBound', 'jQuerySizzleCalls',
+        'headersBiggerThanContent'
+        ],
+
+      }
 
     logstash_connection = TCPSocket.new('192.168.101.2', 5454)
     # logstash_connection = TCPSocket.new('192.168.73.2', 5454)
-    check_result = `phantomas http://#{url}:3000 --format json`
+    url_all = "http://#{url}:3000"
+    check_result = `phantomas #{url_all} --format json`
 
     test_json = valid_json? check_result
     if test_json.first
-      Resque.logger.info "is a valid_json"
+      Resque.logger.info "Is a valid_json"
       json_parser = test_json[1]
+      # p json_parser['offenders'].to_json
+      # exit
       new_hash['metrics'] = json_parser['metrics']
       new_hash['url'] = json_parser['url']
       offenders = json_parser['offenders']
@@ -22,36 +41,45 @@ class MakeStatsJob < ApplicationJob
         case k
         when :time
           v.each do |offender|
-            p extract_value offenders[offender].first
-            extracted_value = extract_value offenders[offender].first
-            new_hash[offender] = {path: extracted_value.first.gsub('http://#{url}:3000',''), time: extracted_value.last}
+            # p offenders[offender].first
+            # extract_value offenders[offender].first
+            # p "ff"
+            offenders[offender].each do |t|
+              extracted_value = extract_value t
+              index_t = offenders[offender].index(t)
+              # p offenders[offender]
+              next if extracted_value.nil?
+              new_hash[offender] = {time: "#{extracted_value.first}-#{index_t}", path: "#{extracted_value.last.gsub(url_all, '')}-#{index_t}"}
+            end
+          end
+        when :orig
+          v.each do |orig_key|
+            new_hash[orig_key] = offenders[orig_key]
           end
         end
+        logstash_connection.write(new_hash.to_json)
       end
+    else
+      Resque.logger.error "Is not a valid json"
     end
-    p new_hash
-    #   test = JSON.parse(check_result).kind_of?(Hash)
-    #   puts "#{Time.now}: Message in progress"
-    #   logstash_connection.write(test)
-    #   logstash_connection.print("hello")
-    #   logstash_connection.close
-    # else
-    #   puts "#{Time.now}: Eror message (not a valid json)"
-    # end
-    # puts "#{Time.now}: Message processed"
+    Resque.logger.info "Message processed"
+    Resque.logger.info "----------------"
   end
+
 
   def valid_json?(json)
     test_parse = JSON.parse(json)
     [true, test_parse]
   rescue
-      [false]
+    [false]
   end
 
   def extract_value(to_split)
-    res_splitted = to_split.match /(.*)\((.*)\)/
-    res_key = res_splitted[2].strip
-    res_val = res_splitted[1].strip
-    [res_key, res_val]
+    res_splitted = to_split.match /(.*)\s\(\D*\s?(\w+\.?\w+).*\)$/
+    if res_splitted
+      res_key = res_splitted[2].strip
+      res_val = res_splitted[1].strip
+      [res_key, res_val]
+    end
   end
 end
